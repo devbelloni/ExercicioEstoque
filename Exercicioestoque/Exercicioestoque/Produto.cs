1f5f015b2edbb53e163fb7ad29fa7139c60f955c
Marcio Belloni <83551398+devbelloni@users.noreply.github.com>
2021-10-20 12:59:21 -0300

    Add files via upload

---
+++ Exercicioestoque/Exercicioestoque/Produto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Produto
+{
+    // acrescentando os métodos
+    public string Nome;
+    public double Preco;
+    public int Quantidade;
+
+
+    // acrescentando as operações
+    public double ValorTotalEmEstoque()
+    {
+        return Preco * Quantidade;
+    }
+
+
+
+    // a função será void, porque a operação não retorna o valor de saída. Ela vai buscar a quantidade do produto e mudar o valor dela. Uma operação que muda apenas a quantidade.
+    // void significa vazio
+    // entrada de função é minúsculo "quantidade"
+    public void AdicionarProdutos(int quantidade)
+    {
+        Quantidade = Quantidade + quantidade;
+        // ou Quantidade +=quantidade
+    }
+
+    // alterado por mim
+    public void RemoverProdutos(int quantidade)
+    {
+        Quantidade -= quantidade;
+    }
+
+    // até aqui
+    // sobreposição: override
+    override public string ToString()
+    {
+        // return Nome + ", $" + Preco.ToString("F2", CultureInfo.InvariantCulture);
+        // nova forma de concatenar
+        return Nome 
+            + ", $" 
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $"
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);  
+
+            }
+}
---
+++ Exercicioestoque/Exercicioestoque/Program.cs
@@ -0,0 +1,35 @@
+﻿// Criando o programa do estoque do curso C# do Udemy
+
+using System.Globalization;
+
+Produto p = new Produto();
+
+Console.WriteLine("Entre os dados do produto.");
+Console.Write("Nome:"); 
+p.Nome=Console.ReadLine();
+Console.Write("Preço:");
+p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Quantidade no estoque:");
+p.Quantidade = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Dados do Produto: "+p.Nome + ",$"+p.Preco ); ficaria muito longo e trabalhoso
+//melhor criar a sob reposição na class e depois usar somente o p. 
+Console.WriteLine(); 
+Console.WriteLine("Dados do Produto: " + p);
+
+Console.WriteLine();
+Console.Write("Digite o número de produtos à ser adicionado ao estoque: ");
+int qte = int.Parse(Console.ReadLine());
+// adicionar a qte ao p por meio do AdicionarProduto
+p.AdicionarProdutos(qte);
+Console.WriteLine();
+Console.WriteLine("Dados atualizados: " +p);
+
+// alterado por mim
+Console.WriteLine();
+Console.Write("Digite o número de produtos à ser removido do estoque: ");
+qte = int.Parse(Console.ReadLine());
+p.RemoverProdutos(qte);
+Console.WriteLine();
+Console.WriteLine("Dados atualizados: " + p);
+
